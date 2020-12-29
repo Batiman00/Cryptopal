343ec30de4e3e1f99e5682f0bbb0a63e11eb0fc6
Batiman00 <69660089+Batiman00@users.noreply.github.com>
2020-12-29 17:12:22 -0300

    Create base64.js

---
+++ base64.js
@@ -0,0 +1,9 @@
+const b64 = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-/'.split("");
+
+const v2hexTo64 = (num = '') => {
+  
+  let total = num.match(/.{1,3}/g).reduce((acc,curr) => acc += parseInt(curr,16).toString(2).padStart(12,0),'');
+  total = total.match(/.{1,6}/g).reduce((acc,curr) => acc += b64[parseInt(curr,2)],'');
+
+  return total;
+};
