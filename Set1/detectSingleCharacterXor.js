716df79c77dc25053bda4da7c7814ad7c60d134b
Batiman00 <69660089+Batiman00@users.noreply.github.com>
2021-01-03 03:47:52 -0300

    Update detectSingleCharacterXor.js

--- Set1/detectSingleCharacterXor.js
+++ Set1/detectSingleCharacterXor.js
@@ -1,20 +1,19 @@
-const b64 = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'.split("");
-
-const score = (a) => {
-    let arr = a.match(/.{1,2}/g).sort();
-    let arr2 = [], avrg = 0;
-    for (let i = 0, j = 0; i < arr.length; i++) {
-        if (arr[i] !== arr[i + 1]) {
-            let n = i - j + 1;
-            arr2.push({ key: arr[i], frequency: n });
+/*Função de analálise de frequência de caracteres em textos*/
+const score = (stringHex) => {
+    var stringHex = stringHex.match(/.{1,2}/g).sort();
+    let arr = [], avrg = 0;
+    for (let i = 0, j = 0; i < stringHex.length; i++) {
+        if (stringHex[i] !== stringHex[i + 1]) {
+            var n = i - j + 1;
+            arr.push({ key: stringHex[i], frequency: n });
             avrg += n;
             j = i + 1;
         }
     }
-    avrg /= arr2.length;
-    let variance = arr2.reduce((acc, curr) => acc += (curr.frequency - avrg) ** 2, 0) / arr2.length;
-    arr2.unshift({ average: avrg, variance: variance, deviation: Math.sqrt(variance) });
-    return arr2;
+    avrg /= stringHex.length;
+    var variance = stringHex.reduce((acc, curr) => acc += (curr.frequency - avrg) ** 2, 0) / stringHex.length;
+    arr.unshift({ average: avrg, variance: variance, deviation: Math.sqrt(variance) });
+    return arr;
 };
-
+/*Função para testar as chaves*/
 const singleByteXor = (text, key) => (text.match(/.{1,2}/g).reduce((acc,curr) => acc += String.fromCharCode(parseInt(curr, 16) ^ key.charCodeAt(0)),''));
