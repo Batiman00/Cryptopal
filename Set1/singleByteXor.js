bdb325be70a2b39640458844a6b9c2803ad0893f
Batiman00 <69660089+Batiman00@users.noreply.github.com>
2021-01-03 03:49:58 -0300

    Update singleByteXor.js

--- Set1/singleByteXor.js
+++ Set1/singleByteXor.js
@@ -1,4 +1,4 @@
 const b64 = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'.split("");
-
+/*Função para testar as chaves*/
 const singleByteXor = (text, key) => (text.match(/.{1,2}/g).reduce((acc,curr) => acc += String.fromCharCode(parseInt(curr, 16) ^ key.charCodeAt(0)),''));
-/*Apenas tente com todas as chaves*/
+/*Tente com todos os caracteres de b64*/
