b3dda0526174e92289bee03b91e4dd58707f1c8f
Batiman00 <69660089+Batiman00@users.noreply.github.com>
2021-01-03 03:42:24 -0300

    Update base64.js

--- Set1/base64.js
+++ Set1/base64.js
@@ -1,8 +1,8 @@
-const b64 = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'.split("");
-
-const v2hexTo64 = (num = '') => {
+/*Função de conversão de base Hexadecimal para base64*/
+const hexTo64 = (num = '') => {
+  const b64 = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/'.split("");
   
-  let total = num.match(/.{1,3}/g).reduce((acc,curr) => acc += parseInt(curr,16).toString(2).padStart(12,0),'');
+  var total = num.match(/.{1,3}/g).reduce((acc,curr) => acc += parseInt(curr,16).toString(2).padStart(12,0),'');
   total = total.match(/.{1,6}/g).reduce((acc,curr) => acc += b64[parseInt(curr,2)],'');
 
   return total;
