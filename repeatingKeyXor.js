a9a449d4b13be774324fe40e01d595220ec08827
Batiman00 <69660089+Batiman00@users.noreply.github.com>
2021-01-03 04:13:32 -0300

    Update repeatingKeyXor.js

--- repeatingKeyXor.js
+++ repeatingKeyXor.js
@@ -1,3 +1,4 @@
-const multByteXor = (text, key) => (text.match(/.{1,2}/g).reduce((acc, curr, index) => acc += String.fromCharCode(parseInt(curr, 16) ^ key[index % key.length].charCodeAt(0)), ''));
-
+/*Função que cifra um texto em hexadecimal, com uma string*/
+const multByteXor = (textHex, key) => (textHex.match(/.{1,2}/g).reduce((acc, curr, index) => acc += String.fromCharCode(parseInt(curr, 16) ^ key[index % key.length].charCodeAt(0)), ''));
+/*Função de conversão*/
 const asciiToHex = (string) => (string.split('').reduce((acc, curr) => acc += curr.charCodeAt(0).toString(16).padStart(2, 0), ''));
