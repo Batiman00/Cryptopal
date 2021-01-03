a84632158502bd3fa4022eb1f29b46920ff127eb
Batiman00 <69660089+Batiman00@users.noreply.github.com>
2021-01-03 03:37:11 -0300

    Update fixedXor.js

--- fixedXor.js
+++ fixedXor.js
@@ -1,2 +1,2 @@
-/*Função que faz o xor de uma string com */
-const xor = (aStringHex, bStringHex) => (aStringHex.split('').reduce((acc,curr,index) => acc += (parseInt(curr,16) ^ parseInt(StringHex[index],16)).toString(16) ,''));
+/*Função que faz o xor de uma string com outra */
+const xor = (aStringHex, bStringHex) => (aStringHex.split('').reduce((acc,curr,index) => acc += (parseInt(curr,16) ^ parseInt(bStringHex[index],16)).toString(16) ,''));
