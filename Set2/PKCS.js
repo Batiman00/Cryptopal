ffcab1be6030bc51b588dcc0fd52c915dcdf3c97
Batiman00 <69660089+Batiman00@users.noreply.github.com>
2021-01-11 12:37:36 -0300

    Update PKCS.js

--- Set2/PKCS.js
+++ Set2/PKCS.js
@@ -1,5 +1,19 @@
-const pkcs = (stringHex, bytes = 16) => {
-    var pad  = bytes - ((stringHex.length / 2) % bytes);
-    pad === 0 ? pad = bytes.toString(16).padStart(2, 0) : pad = pad.toString(16).padStart(2, 0);
-    return stringHex.concat(pad.repeat(parseInt(pad, 16)));
+const pkcs = (string = '', bytes = 16, encode = "ASCII") => {
+
+    encode = encode.toUpperCase()
+    switch (encode) {
+        case "ASCII":
+            var pad = bytes - (string.length % bytes);
+            pad === 0 ? pad = bytes : pad = pad;
+            return string.concat(pad.toString(16).padStart(2, 0).repeat(pad));
+
+        case "HEX":
+            var pad = bytes - ((string.length / 2) % bytes);
+            pad === 0 ? pad = bytes.toString(16).padStart(2, 0) : pad = pad.toString(16).padStart(2, 0);
+            return string.concat(pad.repeat(parseInt(pad, 16)));
+        default:
+            throw new Error(`Invalid encode`);
+    }
+
 }
+module.exports = pkcs;
