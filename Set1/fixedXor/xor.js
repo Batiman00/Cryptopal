a1cb260ea76a4f9b264be92b6657e1e83da9471e
Batiman00 <69660089+Batiman00@users.noreply.github.com>
2021-01-11 11:21:54 -0300

    Update and rename Set1/fixedXor.js to Set1/fixedXor/xor.js

--- Set1/fixedXor/xor.js
+++ Set1/fixedXor/xor.js
@@ -1,2 +1,3 @@
 /*Função que faz o xor de uma string com outra */
 const xor = (aStringHex, bStringHex) => (aStringHex.split('').reduce((acc,curr,index) => acc += (parseInt(curr,16) ^ parseInt(bStringHex[index],16)).toString(16) ,''));
+module.exports = xor;
